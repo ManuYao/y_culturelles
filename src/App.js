b47e3ddad41a50773bf941d124a3d13b7f6b7a42
ManuYao <yaomanuit@gmail.com>
2023-11-11 17:31:49 +0100

    V 0.0.4 | Build PHP AND JS AND API🎗️

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+  import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const App = () => {
+    const [events, setEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-export default App;
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('http://localhost/yCulturelles/YCulturelles.php');
+          if (!response.ok) {
+            throw new Error('La requête a échoué');
+          }
+          const data = await response.json();
+          console.log(data);
+          setEvents(Array.isArray(data) ? data : []); // Check si data est un tableau
+        } catch (err) {
+          setError(err.message);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchData();
+    }, []);
+
+    if (loading) {
+      return <div>Chargement en cours...</div>;
+    }
+
+    if (error) {
+      return <div>Une erreur s'est produite : {error}</div>;
+    }
+
+    // events est un tableau avant de mapper
+    if (!Array.isArray(events)) {
+      return <div>Les données ne sont pas au format attendu</div>;
+    }
+
+    return (
+      //Problème d'affichage
+      <div>
+        {events.map((event) => (
+          <div key={event.slug}>
+            <h2>{event.title_fr}</h2>
+            <p>{event.category_fr}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  export default App;
